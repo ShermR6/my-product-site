d3471e6082adc6cb947d05ee4f2f8b2cbd842331
ShermR6 <andrew.p.sherman21@gmail.com>
2026-02-26 23:20:46 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 export const metadata = {
-  title: "SkyPing",
+  title: "FinalPing",
   description: "Real-time aircraft tracking with proximity alerts.",
 };
 
