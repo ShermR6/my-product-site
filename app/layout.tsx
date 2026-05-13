c8c3a5334b04e3fe88436949852064408189bc4c
ShermR6 <andrew.p.sherman21@gmail.com>
2026-05-13 16:20:36 -0500

    Fix favicon for browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon.ico" },
       { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
     ],
     apple: [{ url: "/apple-touch-icon.png" }],
   },
